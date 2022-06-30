4a8b24318c8cdfb6e2488265becfce3c1860f47b
4teeen <artmvashunin14@gmail.com>
2022-06-30 16:57:55 +0300

    Добавьте файлы проекта.

---
+++ List/List1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class List1<T> : IEnumerable<T>
+    {
+        private const int defCapacity = 4;
+        private T[] items;
+        static readonly T[] emptyArray = new T[0];
+        private int size;
+
+        public List1()
+        {
+            items = emptyArray;
+        }
+
+        public int Count
+        {
+            get { return size; }
+        }
+
+        public int Capacity
+        {
+            get { return items.Length; }
+            set
+            {
+                if (value < size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                if (value != items.Length)
+                {
+                    if (value > 0)
+                    {
+                        T[] newItems = new T[value];
+                        if (size > 0)
+                        {
+                            Array.Copy(items, 0, newItems, 0, size);
+                        }
+
+                        items = newItems;
+                    }
+                    else
+                    {
+                        items = emptyArray;
+                    }
+                }
+            }
+        }
+
+        private void EnsureCapacity(int min) 
+        {
+            if (items.Length < min)
+            {
+                int newCapacity;
+                if (items.Length == 0)
+                {
+                    newCapacity = defCapacity;
+                }
+                else 
+                {
+                    newCapacity = items.Length * 2;
+                }
+                if ((uint)newCapacity > Int32.MaxValue) newCapacity = Int32.MaxValue;
+                if (newCapacity < min) newCapacity = min;
+                Capacity = newCapacity;
+            }
+        }
+
+
+        public void Add(T item)
+        {
+            if (size == items.Length)
+                EnsureCapacity(size + 1);
+            items[size++] = item;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new List1IEnumerator<T>(items, size);
+
+        }
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List1<int> list = new List1<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            foreach(var item in list)
+            {
+                Console.Write(item+" ");
+            }    
+        }
+    }
+}
